28c8cdb7b799116ae6562bf646d85262eb9d5a3f
Yegon Kipkirui Geoffrey <52694586+ipkiruiYegon@users.noreply.github.com>
2021-11-19 09:55:54 +0300

    add create update and delete operation to user model

---
+++ app/crud.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+
+from . import model, schemas
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def get_user(db: Session, user_id: int):
+    return db.query(model.User).filter(model.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(model.User).filter(model.User.email == email).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(model.User).filter(model.User.username == username).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(model.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = pwd_context.hash(user.password)
+    db_user = model.User(email=user.email, password=hashed_password, username=user.username,firstname=user.firstname,othernames=user.othernames)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
