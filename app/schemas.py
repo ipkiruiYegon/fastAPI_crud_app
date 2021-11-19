ebc9ef6da7ddf341c93c90621b4f313cc2af1b3e
Yegon Kipkirui Geoffrey <52694586+ipkiruiYegon@users.noreply.github.com>
2021-11-19 09:51:20 +0300

    add User pydantic schema

---
+++ app/schemas.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    email: str
+    username: str
+    firstname: str
+    othernames: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    email: str
+    is_active: bool
+    last_login: str
+
+    class Config:
+        orm_mode = True
